6ebee2fce727f9385a02b75e3f62e2e8f96f11bc
adesina <adesinaoluwayanmife7@gmail.com>
2023-05-30 06:17:37 +0100

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+export const environment = {
+               production: false,
+               weatherApiBaseUrl: "https://open-weather13.p.rapidapi.com",
+
+               XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
+
+               XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com ',
+
+               XRapidAPIKeyHeaderName: 'X-RapidAPI-Key',
+
+               XRapidAPIKeyHeaderValue: '8e3c3edf6emshae470b211815886p16eaccjsn9e978db433dd',
+
+}
+
